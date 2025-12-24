97fbd42f5d0629d3091ee886b8c280ea789a4131
Ahmedkhan-web <a.khan.786.786.786.7@gmail.com>
2025-12-24 11:38:01 -0800

    alert string

---
+++ alert , strings and operators/app.js
@@ -0,0 +1,37 @@
+// NO 1 = Alert
+
+// 1 
+var userName = prompt("Enter your name");
+alert("Well Come " + userName);
+
+
+
+
+// 2
+var username = prompt("Enter your name");
+var userAge = prompt("Enter your age");
+alert(username + " is " + userAge + " years old");
+
+
+
+
+// 3
+var favColor = prompt("Enter your favourite color")
+alert("WoW ! " + favColor + " is a nice color");
+
+
+
+
+// 4
+var num1 = Number(prompt("Enter yor first Number"))
+var num2 = Number(prompt("Enter your last number"))
+alert("The result of " + num1 + " and " + num2 + " is " + (num1 + num2));
+
+
+
+
+// 5
+var yourName = prompt("Enter your name")
+var yourHobby = prompt("Enter your hobby")
+var yourCountry = prompt("Enter your Country")
+alert("Hi ! my name is " + yourName + " from " + yourCountry + " And I like " + yourHobby);
