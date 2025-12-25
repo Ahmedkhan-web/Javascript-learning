3d78eeb56301f69a24ab4ce8fb1b294c0d65a041
Ahmedkhan-web <a.khan.786.786.786.7@gmail.com>
2025-12-25 00:29:41 -0800

    string and operators

--- alert , strings and operators/app.js
+++ alert , strings and operators/app.js
@@ -1,37 +1,258 @@
-// NO 1 = Alert
+// // NO 1 = Alert
 
-// 1 
-var userName = prompt("Enter your name");
-alert("Well Come " + userName);
-
-
-
-
-// 2
-var username = prompt("Enter your name");
-var userAge = prompt("Enter your age");
-alert(username + " is " + userAge + " years old");
+// // 1 
+// var userName = prompt("Enter your name");
+// alert("Well Come " + userName);
 
 
 
 
-// 3
-var favColor = prompt("Enter your favourite color")
-alert("WoW ! " + favColor + " is a nice color");
+// // 2
+// var username = prompt("Enter your name");
+// var userAge = prompt("Enter your age");
+// alert(username + " is " + userAge + " years old");
 
 
 
 
-// 4
-var num1 = Number(prompt("Enter yor first Number"))
-var num2 = Number(prompt("Enter your last number"))
-alert("The result of " + num1 + " and " + num2 + " is " + (num1 + num2));
+// // 3
+// var favColor = prompt("Enter your favourite color")
+// alert("WoW ! " + favColor + " is a nice color");
 
 
 
 
-// 5
-var yourName = prompt("Enter your name")
-var yourHobby = prompt("Enter your hobby")
-var yourCountry = prompt("Enter your Country")
-alert("Hi ! my name is " + yourName + " from " + yourCountry + " And I like " + yourHobby);
+// // 4
+// var num1 = Number(prompt("Enter yor first Number"))
+// var num2 = Number(prompt("Enter your last number"))
+// alert("The result of " + num1 + " and " + num2 + " is " + (num1 + num2));
+
+
+
+
+// // 5
+// var yourName = prompt("Enter your name")
+// var yourHobby = prompt("Enter your hobby")
+// var yourCountry = prompt("Enter your Country")
+// alert("Hi ! my name is " + yourName + " from " + yourCountry + " And I like " + yourHobby);
+
+
+
+
+
+
+// NO  2
+// String concatenation
+
+
+// // 1
+// var street = prompt("Enter your street")
+// var City = prompt("Enter your City")
+// var Country = prompt("Enter your Country")
+// console.log("Your Full Address is: " + street + " " + City + " " + Country);
+
+
+
+// // 2
+// var userFullName = prompt("Enter your full name");
+// var FavNum = prompt("Enter your favourite number");
+// console.log("Your New User Name Is " + userFullName + FavNum);
+
+
+
+// // 3
+// var firstN = prompt("Enter Your First Nmae");
+// var lastN = prompt("Enter Your Last Nmae");
+// console.log(firstN + "." + lastN + "@gmail.com");
+
+
+
+// // 4
+// var favouriteColor = prompt("Enter Your Favourite Color");
+// var favouriteAnimal = prompt("Enter Your Favourite Animal");
+// var favouriteFood = prompt("Enter Your Favourite Food");
+// console.log("Your Favourite Things Are: " + favouriteColor + " , " + favouriteAnimal + " , " +favouriteFood)
+
+
+
+
+// // 5
+// var quate = prompt("Enter a quate");
+// var AuthorName = prompt("Enter the author's name");
+// console.log( quate +  " -" + AuthorName );
+
+
+
+
+
+
+
+
+
+
+// NO 3
+// Operators
+
+
+
+// 15 !== 15 && 8 * 2 !== 20 || 'hello' === 'world' && 10 > 15 || 6 === '6'
+// false && true || false && false || false
+// false || false || false
+// false
+
+// 9 > 8 && 3 + 7 === 11 || 4 !== '4' && 10 - 2 === 8 || 5 * 3 === 15
+// true && false || true && true || true
+// false || true || true
+// true
+
+// 0 === false && 7 * 2 === 14 || 'True' === true && 5 !== '5' || 6 + 6 === 12
+// false && true || false && true || true
+// false || false || true
+// true
+
+// 'a' !== 'A' && 3 * 3 === 9 || 4 < 4 && 10 / 2 === 5 || 8 === '8'
+// true && true || false && true || false
+// true || false || false
+// true
+
+// 100 / 10 === 10 && 7 === 7 || 3 * 2 !== 7 && 'hello' === 'hello' || 9 > 12
+// true && true || true && true || false
+// true || true || false
+// true
+
+// 20 / 4 === 5 && 3 + 5 === 8 || 8 === 8 && 'yes' !== 'YES' || 12 === '12'
+// true && true || true && true || false
+// true || true || false
+// true
+
+// 6 > 5 && 4 * 2 !== 10 || 7 === 6 + 1 && 'no' === 'yes' || 100 !== 100
+// true && true || true && false || false
+// true || false || false
+// true
+
+// 5 === '5' || 8 / 2 !== 5 && 4 + 6 === 11 || 9 === 8 + 1 && 'abc' !== 'ABC'
+// false || false && false || true && true
+// false || false || true
+// true
+
+// true && false || 4 * 2 === 8 && 'hello' === 'hello' || 5 !== '5' && 3 < 5
+// false || true && true || true && true
+// false || true || true
+// true
+
+// false && true || 'red' === 'blue' && 5 + 5 === 10 || 6 === '6' && 9 > 8
+// false || false && true || false && true
+// false || false || false
+// false
+
+// 'dog' === 'dog' && 3 * 3 === 9 || false && 5 === 5 || 7 === '7'
+// true && true || false && true || false
+// true || false || false
+// true
+
+// true || false && 4 * 4 === 16 || 'cat' !== 'dog' && 3 < 5 || 10 / 2 === 5
+// true || false || true && true || true
+// true
+
+// false && true || 5 * 3 === 15 || 6 > 8 && 7 + 1 === 8 || 'test' === 'test'
+// false || true || false && true || true
+// true
+
+// true && false || 8 / 2 === 4 && 3 + 2 === 5 || 9 !== 9 && 'green' === 'green'
+// false || true && true || false && true
+// true
+
+// false && 5 > 10 || 7 === '7' && 4 * 5 === 20 || true && false
+// false || false && true || false
+// false
+
+// true && 'hello' === 'hello' || 9 * 2 === 18 && 6 + 2 === 8 || false && 7 > 3
+// true || true && true || false
+// true
+
+// 'apple' === 'apple' && 2 + 3 === 5 || false && true || 10 - 5 === 5 || 5 === '5'
+// true && true || false || true || false
+// true
+
+// 15 * 2 !== 30 && 5 === 5 || 'cat' === 'dog' && 7 / 2 === 3.5 || 20 > 10
+// false && true || false && true || true
+// false || false || true
+// true
+
+// 8 * 3 === 24 && 100 / 10 === 10 || 'red' !== 'green' && 9 + 1 === 10 || 5 === 5
+// true && true || true && true || true
+// true
+
+// 10 > 8 && 7 * 2 === 14 || 3 - 1 === 2 && 'sun' === 'sun' || 'hello' !== 'HELLO'
+// true && true || true && true || true
+// true
+
+// 0 !== '0' && 9 + 5 === 14 || 3 * 3 === 9 && 'apple' === 'apple' || 12 / 3 === 4
+// true && true || true && true || true
+// true
+
+// 'hi' !== 'HI' && 7 + 2 === 9 || 10 / 2 === 5 && 'bar' !== 'bar' || 3 > 4
+// true && true || true && false || false
+// true
+
+// 100 / 5 === 20 && 'dog' !== 'dog' || 12 * 2 === 24 && 9 === 9 || 'red' === 'blue'
+// true && false || true && true || false
+// true
+
+// 'true' !== true && 4 + 6 === 10 || 8 / 4 === 2 && 15 - 5 === 10 || 'yes' === 'YES'
+// true && true || true && true || false
+// true
+
+// 5 * 5 === 25 && 3 - 1 === 2 || 20 !== '20' && 10 === 10 || 'test' !== 'TEST'
+// true && true || true && true || true
+// true
+
+// 'cat' !== 'dog' && 4 * 5 === 20 || 7 + 2 === 9 && 2 !== 1 || 12 / 3 === 4
+// true && true || true && true || true
+// true
+
+// 9 > 8 && 6 * 2 === 12 || 3 + 7 !== 10 && 'apple' === 'apple' || 6 === '6'
+// true && true || false && true || false
+// true
+
+// 'hello' === 'hello' && 4 * 3 === 12 || 10 / 5 === 2 && 5 !== 5 || 'world' !== 'WORLD'
+// true && true || true && false || true
+// true
+
+// 20 - 5 === 15 && 'a' !== 'A' || 3 * 2 === 6 && 5 === '5' || 7 > 6
+// true && true || true && false || true
+// true
+
+// 4 + 4 === 8 && 6 * 3 === 18 || 5 !== '5' && 'dog' === 'dog' || 12 === 12
+// true && true || true && true || true
+// true
+
+// 'false' === 'false' && 10 - 4 === 6 || 3 / 3 === 1 && 7 === 7 || 8 * 2 === 16
+// true && true || true && true || true
+// true
+
+// 'blue' !== 'green' && 9 + 6 === 15 || 5 === 5 && 8 / 4 === 2 || 'apple' === 'banana'
+// true && true || true && true || false
+// true
+
+// 8 * 2 === 16 && 5 + 4 === 9 || 6 === '6' && 10 - 3 === 7 || 7 * 2 !== 16
+// true && true || false && true || true
+// true
+
+// 5 + 2 === 7 && 'red' === 'red' || 3 * 4 === 12 && 'dog' !== 'cat' || 10 - 5 === 5
+// true && true || true && true || true
+// true
+
+// 7 * 3 === 21 && 12 / 4 === 3 || 5 + 6 === 11 && 'blue' !== 'green' || 'yes' !== 'YES'
+// true && true || true && true || true
+// true
+
+// 'false' === false && 4 * 2 === 8 || 3 - 2 === 1 && 10 / 5 === 2 || 'a' !== 'A'
+// false && true || true && true || true
+// true
+
+// 6 + 6 === 12 && 'yellow' === 'yellow' || 8 !== 8 && 3 * 3 === 9 || 'hello' !== 'HELLO'
+// true && true || false && true || true
+// true
+
+
